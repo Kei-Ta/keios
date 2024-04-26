475aa9ba684ca183d363ef11508087bbc5b0f394
Kei-Ta <tktkhome@icloud.com>
2024-04-26 18:00:03 +0900

    day5b

---
+++ kernel/.graphics.d
@@ -0,0 +1,3 @@
+graphics.o: graphics.cpp graphics.hpp frame_buffer_config.hpp \
+  /home/kei/osbook/devenv/x86_64-elf/include/c++/v1/stdint.h \
+  /home/kei/osbook/devenv/x86_64-elf/include/c++/v1/__config
---
+++ kernel/.main.d
@@ -0,0 +1,7 @@
+main.o: main.cpp \
+  /home/kei/osbook/devenv/x86_64-elf/include/c++/v1/cstdint \
+  /home/kei/osbook/devenv/x86_64-elf/include/c++/v1/__config \
+  /home/kei/osbook/devenv/x86_64-elf/include/c++/v1/stdint.h \
+  /home/kei/osbook/devenv/x86_64-elf/include/c++/v1/cstddef \
+  /home/kei/osbook/devenv/x86_64-elf/include/c++/v1/version \
+  frame_buffer_config.hpp graphics.hpp font.hpp
